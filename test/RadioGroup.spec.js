cfc3784a28b2303c6b57f00d0829f0b4fd80f1c4
Matthew Watts <wattsm2008@outlook.com>
2019-10-06 12:58:33 -0500

    Updated tests

--- test/RadioGroup.spec.js
+++ test/RadioGroup.spec.js
@@ -3,16 +3,36 @@ import ReduxFormMaterialUIRadioGroup from "../src/RadioGroup"
 import noop from "lodash.noop"
 import renderer from "react-test-renderer"
 
-describe("RadioGroup", () => {
-    it("renders a RadioGroup", () => {
+describe( "RadioGroup", () => {
+    it( "renders a RadioGroup", () => {
         const component = renderer.create(
             <ReduxFormMaterialUIRadioGroup input={{ onChange: noop, name: "myRadio", value: "Foo" }} meta={{}} />
         )
         let tree = component.toJSON()
-        expect(tree).toMatchSnapshot()
-    })
+        expect( tree ).toMatchSnapshot()
+    } )
 
-    it("renders a RadioGroup with no error when not touched", () => {
+    it( 'renders a RadioGroup with helper text when not touched', () => {
+        const component = renderer.create(
+            <ReduxFormMaterialUIRadioGroup input={{ onChange: noop, name: "myRadio", value: "Foo" }} meta={{}}
+                helperText='My Helper Text' />
+        )
+        let tree = component.toJSON()
+        expect( tree ).toMatchSnapshot()
+    } )
+
+    it( 'renders a RadioGroup with error when touched and helper text', () => {
+        const component = renderer.create(
+            <ReduxFormMaterialUIRadioGroup
+                input={{ onChange: noop, name: "myRadio", value: "Foo" }}
+                meta={{ error: 'FooError', touched: true }}
+                helperText='My Helper Text' />
+        )
+        let tree = component.toJSON()
+        expect( tree ).toMatchSnapshot()
+    } )
+
+    it( "renders a RadioGroup with no error when not touched", () => {
         const component = renderer.create(
             <ReduxFormMaterialUIRadioGroup
                 input={{ onChange: noop, name: "myRadio", value: "Foo" }}
@@ -20,10 +40,10 @@ describe("RadioGroup", () => {
             />
         )
         let tree = component.toJSON()
-        expect(tree).toMatchSnapshot()
-    })
+        expect( tree ).toMatchSnapshot()
+    } )
 
-    it("renders a RadioGroup with an error when touched", () => {
+    it( "renders a RadioGroup with an error when touched", () => {
         const component = renderer.create(
             <ReduxFormMaterialUIRadioGroup
                 input={{ onChange: noop, name: "myRadio", value: "Foo" }}
@@ -31,10 +51,10 @@ describe("RadioGroup", () => {
             />
         )
         let tree = component.toJSON()
-        expect(tree).toMatchSnapshot()
-    })
+        expect( tree ).toMatchSnapshot()
+    } )
 
-    it("renders a RadioGroup with no warning when not touched", () => {
+    it( "renders a RadioGroup with no warning when not touched", () => {
         const component = renderer.create(
             <ReduxFormMaterialUIRadioGroup
                 input={{ onChange: noop, name: "myRadio", value: "Foo" }}
@@ -42,10 +62,10 @@ describe("RadioGroup", () => {
             />
         )
         let tree = component.toJSON()
-        expect(tree).toMatchSnapshot()
-    })
+        expect( tree ).toMatchSnapshot()
+    } )
 
-    it("renders a RadioGroup with a warning when touched", () => {
+    it( "renders a RadioGroup with a warning when touched", () => {
         const component = renderer.create(
             <ReduxFormMaterialUIRadioGroup
                 input={{ onChange: noop, name: "myRadio", value: "Foo" }}
@@ -53,10 +73,10 @@ describe("RadioGroup", () => {
             />
         )
         let tree = component.toJSON()
-        expect(tree).toMatchSnapshot()
-    })
+        expect( tree ).toMatchSnapshot()
+    } )
 
-    it("should ignore defaultValue", () => {
+    it( "should ignore defaultValue", () => {
         const component = renderer.create(
             <ReduxFormMaterialUIRadioGroup
                 input={{ onChange: noop, name: "myRadio", value: "Foo" }}
@@ -65,6 +85,6 @@ describe("RadioGroup", () => {
             />
         )
         let tree = component.toJSON()
-        expect(tree).toMatchSnapshot()
-    })
-})
+        expect( tree ).toMatchSnapshot()
+    } )
+} )
