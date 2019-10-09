b4e7b061e7feff7a57f3037690a861dae1fb6be4
Matthew Watts <wattsm2008@outlook.com>
2019-10-08 21:04:07 -0500

    Updated remaining test suites

--- src/Switch.jsx
+++ src/Switch.jsx
@@ -1,17 +1,20 @@
-import FormControlLabel from "@material-ui/core/FormControlLabel"
-import React from "react"
-import Switch from "@material-ui/core/Switch"
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import React from 'react';
+import Switch from '@material-ui/core/Switch';
 
 export default function ReduxFormMaterialUISwitch({
     input: { value, onChange, ...inputProps },
     defaultChecked,
+    checked,
     ...props
 }) {
     return (
         <FormControlLabel
-            control={<Switch checked={value ? true : false} onCheck={onChange} />}
+            control={
+                <Switch checked={value ? true : false} onChange={onChange} />
+            }
             {...props}
             {...inputProps}
         />
-    )
+    );
 }
