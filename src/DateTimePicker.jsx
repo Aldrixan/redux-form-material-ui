0a612241d457621bdca2990a3fd76d561bd815cf
Matthew Watts <matthew.watts@purplewave.com>
2019-10-17 16:56:41 -0500

    Started work on date picker

---
+++ src/DateTimePicker.jsx
@@ -0,0 +1,27 @@
+import { KeyboardDatePicker, KeyboardTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers"
+
+import DateFnsUtils from "@date-io/date-fns"
+import React from "react"
+
+export default function ReduxFormMaterialUITextField({
+    label,
+    input: { onChange, ...inputProps },
+    meta: { touched, invalid, error, warning },
+    defaultValue,
+    helperText,
+    ...custom
+}) {
+    return (
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+                label={label}
+                placeholder={label}
+                error={touched && invalid}
+                helperText={(touched && (error || warning)) || helperText}
+                onChange={(e, val) => onChange(val)}
+                {...inputProps}
+                {...custom}
+            />
+        </MuiPickersUtilsProvider>
+    )
+}
