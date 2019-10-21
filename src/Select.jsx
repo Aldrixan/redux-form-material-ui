441bab646b8fd2442dd8c7e93ea08d14a8079a6b
Matthew Watts <matthew.watts@purplewave.com>
2019-10-21 13:25:56 -0500

    Started work on styling to control label placement

--- src/Select.jsx
+++ src/Select.jsx
@@ -3,6 +3,22 @@ import InputLabel from '@material-ui/core/InputLabel';
 import React from 'react';
 import Select from '@material-ui/core/Select';
 import {getHelperText} from './utils';
+import cx from 'classnames';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexDirection: 'row',
+        alignItems: 'flex-start'
+    },
+    label: {
+        position: 'relative',
+        paddingRight: theme.spacing
+    },
+    base: {
+        flexGrow: 1
+    }
+}))
 
 export default function ReduxFormMaterialUISelect({
     input: { name, ...input },
@@ -11,6 +27,7 @@ export default function ReduxFormMaterialUISelect({
     helperText,
     children,
     className,
+    labelPlacement,
     ...props
 }) {
     /*
@@ -25,10 +42,11 @@ export default function ReduxFormMaterialUISelect({
         On InputBase:
         flex-grow: 1
     */
+   const classes = useStyles();
     return (
-        <FormControl error={touched && !!error} className={className}>
-            <InputLabel htmlFor={name}>{props.label}</InputLabel>
-            <Select {...input} {...props}>
+        <FormControl error={touched && !!error} className={cx(className, labelPlacement === 'left' ? classes.root : ''}>
+            <InputLabel htmlFor={name} className={labelPlacement === 'left' ? classes.label : ''}>{props.label}</InputLabel>
+            <Select {...input} {...props} className={labelPlacement === 'left' ? classes.base : ''}>
                 {children}
             </Select>
             {getHelperText(touched, error, warning, helperText)}
