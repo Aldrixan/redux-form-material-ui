f47749291b349f610378de0849cb5c19917f741d
Matthew Watts <matthew.watts@purplewave.com>
2019-11-06 08:45:48 -0600

    Add working tests to slider component

--- test/Slider.spec.js
+++ test/Slider.spec.js
@@ -1,55 +1,56 @@
-import { render, unmountComponentAtNode } from 'react-dom';
+import { render, unmountComponentAtNode } from "react-dom"
 
 import React from "react"
 import ReduxFormMaterialUISlider from "../src/Slider"
-import { act } from 'react-dom/test-utils';
+import { act } from "react-dom/test-utils"
 import noop from "lodash.noop"
 
 // import renderer from "react-test-renderer"
 
-describe( "Switch", () => {
-    let container = null;
-    beforeEach( () => {
-        container = document.createElement( 'div' );
-        document.body.appendChild( container );
-    } )
+describe("Switch", () => {
+    let container = null
+    beforeEach(() => {
+        container = document.createElement("div")
+        document.body.appendChild(container)
+    })
 
-    afterEach( () => {
-        unmountComponentAtNode( container );
-        container.remove();
-        container = null;
-    } );
+    afterEach(() => {
+        unmountComponentAtNode(container)
+        container.remove()
+        container = null
+    })
 
-    test( 'renders an unchecked Slider', () => {
-        act( () => {
-            render( <ReduxFormMaterialUISlider input={{ onChange: noop, name: "mySlider" }} />, container );
-        } )
-        let element = container.querySelector( 'input[name="mySlider"]' );
-        expect( element ).toBeDefined();
-        expect( element.checked ).toBeFalsy()
-    } )
+    test("renders a Slider", () => {
+        act(() => {
+            render(
+                <ReduxFormMaterialUISlider
+                    input={{ onChange: noop, name: "mySlider", value: 5 }}
+                    meta={{}}
+                    label="My Slider"
+                    helperText="My Helper Text"
+                />,
+                container
+            )
+        })
+        let element = container.querySelector('input[name="mySlider"]')
+        expect(element).toBeDefined()
+        expect(element.getAttribute('value')).toEqual("5");
+    })
 
-    test( 'renders a checked Slider', () => {
-        act( () => {
-            render( <ReduxFormMaterialUISlider input={{ value: true, onChange: noop, name: "mySlider" }} />, container );
-        } )
-        let element = container.querySelector( 'input[name="mySlider"]' );
-        expect( element.checked ).toBeTruthy()
-    } )
-
-    test( 'should ignore checked', () => {
-        act( () => {
-            render( <ReduxFormMaterialUISlider input={{ onChange: noop, name: "mySlider" }} checked={true} />, container );
-        } )
-        let element = container.querySelector( 'input[name="mySlider"]' );
-        expect( element.checked ).toBeFalsy()
-    } )
-
-    test( 'should ignore defaultValue', () => {
-        act( () => {
-            render( <ReduxFormMaterialUISlider input={{ onChange: noop, name: "mySlider" }} defaultValue={true} />, container );
-        } )
-        let element = container.querySelector( 'input[name="mySlider"]' );
-        expect( element.checked ).toBeFalsy();
-    } )
-} )
+    test("should ignore defaultValue", () => {
+        act(() => {
+            render(
+                <ReduxFormMaterialUISlider
+                    input={{ onChange: noop, name: "mySlider", value: 5 }}
+                    meta={{}}
+                    defaultValue={10}
+                    label="My Slider"
+                    helperText="My Helper Text"
+                />,
+                container
+            )
+        })
+        let element = container.querySelector('input[name="mySlider"]')
+        expect(element.getAttribute('value')).toEqual("5");
+    })
+})
